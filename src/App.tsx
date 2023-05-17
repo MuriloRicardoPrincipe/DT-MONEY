a87a73a96e1ed3c83126be3ae8b773b820088355
Murilo Ricardo Principe <murilo.rprincipe2021@outlook.com>
2023-05-17 09:02:07 -0300

    finalizado estilos

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/defaut";
+import { GlobalStyle } from "./styles/global";
+import { Transactions } from "./pages/Transactions";
 
 
 export function App() {
 
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>Hello Word</h1>
+      <GlobalStyle/>
+      <Transactions/>
     </ThemeProvider>
   )
 }
